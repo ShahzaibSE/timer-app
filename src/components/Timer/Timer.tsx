9a5ab958239b55b92bb0e3bca10c1deb1f79e8fb
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-09-25 20:12:32 +0500

    UI updated.

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 // Material.
 import Button from "@material-ui/core/Button"
 // Assets.
 import "./Timer.css"
 
 const Timer = () => {
+    const [timeLeft, setTimeLeft] = useState<Number>(60)
+    // Format time.
     const formatTime = (time:any) => {
         // The largest round integer less than or equal to the result of time divided being by 60.
         const minutes = Math.floor(time / 60);
@@ -24,12 +26,12 @@ const Timer = () => {
     return (
         <div className="base-timer">
             <svg className="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <g className="base-timer__circle">
-                <circle className="base-timer__path-elapsed" cx="50" cy="50" r="45" />
-                </g>
+            <g className="base-timer__circle">
+                <circle className="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+            </g>
             </svg>
-            <span>
-                {/* <!-- Remaining time label --> */}
+            <span id="base-timer-label" className="base-timer__label">
+                {formatTime(timeLeft)}
             </span>
         </div>
     )
