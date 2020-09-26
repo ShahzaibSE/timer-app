6ec787c792d9faaced9401dcf1a55a3849685650
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-09-26 20:45:25 +0500

    Created timer component and added animation.

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,22 +1,36 @@
 import React, {useState} from 'react'
-// Material.
-import Button from "@material-ui/core/Button"
+// Animation utils.
+import {AnimationWrapper} from "react-hover-animation"
 // Assets.
 import "./Timer.css"
-import { interval } from 'rxjs'
 
+ // Ring CSS config.,
+ const COLOR_CODES = {
+    info: {
+      color: "green"
+    }
+};
+
+const FULL_DASH_ARRAY = 283;
+const TIME_LIMIT = 20;  
+// =============== Timer Component ============= //
 const Timer = () => {
     // let [timeLeft, setTimeLeft] = useState(60)
     let [isOn, setIsOn] = useState(false)
-    let [seconds, setSeconds] = useState(60.0)
+    let [seconds, setSeconds] = useState(0.0)
     let [intervalID, setIntervalID] = useState(0)
+    //
+    // CSS colors for ring.
+    let remainingPathColor = COLOR_CODES.info.color;
+    //
     // Format time.
     const formatTime = (time:any) => {
         // The largest round integer less than or equal to the result of time divided being by 60.
         const minutes = Math.floor(time / 60);
         
         // Seconds are the remainder of the time divided by 60 (modulus operator)
-        setSeconds(time % 60);
+        // setSeconds(time % 60);
+        let seconds = time % 60;
         let seconds_formated:any = seconds
         
         // If the value of seconds is less than 10, then display seconds with a leading zero
@@ -27,6 +41,17 @@ const Timer = () => {
         // The output in MM:SS format
         return `${minutes}:${seconds_formated}`;
     }
+    //
+    // Circle decreaser.
+    const setCircleDashArray = () => {
+        const circleDasharray = `${(
+                    seconds * FULL_DASH_ARRAY
+                    ).toFixed(0)} 283`;
+        //
+        let base_timer_path_remaining_element: any|null = document.getElementById("base-timer-path-remaining");
+        base_timer_path_remaining_element.setAttribute("strokeDasharray", circleDasharray);
+      }
+    // setCircleDashArray()  
     //
     // Time features.
     const startTimer = () => {
@@ -58,14 +83,32 @@ const Timer = () => {
     //
     return (
         <div className="base-timer">
-            <svg className="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <div>
+            <svg className="base-timer__svg base_timer_circle_internal" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g className="base-timer__circle">
-                <circle className="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-            </g>
-            </svg>
-            <span id="base-timer-label" className="base-timer__label">
-                {formatTime(seconds)}
-            </span>
+                    <circle className="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                    <path
+                        id="base-timer-path-remaining"
+                        strokeDasharray="283"
+                        className={`base-timer__path-remaining ${remainingPathColor}`}
+                        d="
+                        M 50, 50
+                        m -45, 0
+                        a 45,45 0 1,0 90,0
+                        a 45,45 0 1,0 -90,0
+                        "
+                    ></path>
+                </g>
+                </svg>
+                    <span id="base-timer-label" className="base-timer__label" style={{fontWeight:"bold"}}>
+                            <AnimationWrapper config={{
+                                transform:{initial:'scale(1)',onHover:'scale(1.5)'},
+                                opacity: {initial:'1',onHover:'1'}
+                                }}>
+                            {formatTime(seconds)}
+                        </AnimationWrapper>
+                    </span>
+            </div>
         </div>
     )
 }
